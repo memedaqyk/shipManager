2d8ffdd5b527adcbe9d0b832ffc286ef7d09b521
memedaqyk <qianyongkang12345@gmail.com>
2017-03-23 22:01:02 +0800

    google to amap

--- js/ship-manager.js
+++ js/ship-manager.js
@@ -1,7 +1,13 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 31.86, lng: 117.27},
-    zoom: 8
-  });
-}
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 31.86, lng: 117.27},
+//     zoom: 8
+//   });
+// }
+
+var map = new AMap.Map('container',{
+	resizeEnable: true,
+	zoom: 10,
+	center: [116.480983, 40.0958]
+});
