3df1b4bede0deceb8243c7eb9f1df9320ecdab30
memedaqyk <qianyongkang12345@gmail.com>
2017-03-26 20:22:14 +0800

    to js for arcgis

--- js/ship-manager.js
+++ js/ship-manager.js
@@ -1,13 +1,9 @@
-// var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 31.86, lng: 117.27},
-//     zoom: 8
-//   });
-// }
-
-var map = new AMap.Map('container',{
-	resizeEnable: true,
-	zoom: 10,
-	center: [116.480983, 40.0958]
+require(["esri/map", "dojo/domReady!"], function(Map) {
+  var map = new Map("map", {
+    center: [-118, 34.5],
+    zoom: 8,
+    basemap: "topo"
+  });
 });
+
+var body_height = document.getElementById("map");
