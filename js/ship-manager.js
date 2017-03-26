27f62c12d35b0c241fa016de734b1ddc3d396547
memedaqyk <qianyongkang12345@gmail.com>
2017-03-26 20:44:20 +0800

    bug the height

--- js/ship-manager.js
+++ js/ship-manager.js
@@ -1,9 +1,11 @@
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  var map = new Map("map", {
+require(['esri/map', 'dojo/domReady!'], function(Map) {
+  var map = new Map('map', {
     center: [-118, 34.5],
     zoom: 8,
-    basemap: "topo"
+    basemap: 'topo'
   });
 });
 
-var body_height = document.getElementById("map");
+var _map = document.getElementById('map');
+_map.style.height = document.body.clientHeight - 45 + 'px';
+console.log(document.body.clientHeight);
