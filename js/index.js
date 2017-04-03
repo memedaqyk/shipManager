230b2327ff60e3ec7b0bee496f3cbc55ffa86468
memedaqyk <qianyongkang12345@gmail.com>
2017-04-03 14:54:09 +0800

    pop up

---
+++ js/index.js
@@ -0,0 +1,4 @@
+
+var _main = document.getElementById('ship');
+
+_main.style.height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 40 + 'px';
--- js/ship-manager.js
+++ js/ship-manager.js
@@ -1,11 +1,54 @@
-require(['esri/map', 'dojo/domReady!'], function(Map) {
+require(['esri/map',  'esri/layers/FeatureLayer','dojo/domReady!'], function(Map, FeatureLayer) {
   var map = new Map('map', {
     center: [-118, 34.5],
     zoom: 8,
     basemap: 'topo'
   });
+
+
 });
 
-var _map = document.getElementById('map');
-_map.style.height = document.body.clientHeight - 45 + 'px';
-console.log(document.body.clientHeight);
+//设置内容高度等于页面高度
+var content = document.getElementsByClassName('content')[0];
+content.style.height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 45 + 'px';
+//设置左边部分等于页面高度
+var mainLeft = document.getElementsByClassName('main-left')[0];
+mainLeft.style.height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 45 + 'px';
+
+var mainLeftLi = document.getElementsByClassName('left-ul')[0];
+var list = mainLeftLi.children;
+
+
+function showInfo() {
+	var bottomInfo = document.getElementsByClassName('bottom-info')[0];
+	var table = document.createElement('table');
+	table.style.border = '1px';
+	// table.style.width = '400px';
+	table.style.borderCollapse = 'collapse';
+	table.style.borderColor = '#ccc';
+	table.innerHTML = '<tr><th>haha</th><th>hahaha</th></tr><tr><td>chuan</td><td>gao</td></tr>';
+	bottomInfo.appendChild(table);
+}
+list[0].addEventListener('click', showInfo, false);
+
+
+
+  $( function() {
+    $( '#dialog' ).dialog({
+      autoOpen: false,
+      show: {
+        effect: 'blind',
+        duration: 600
+      },
+      hide: {
+        effect: 'explode',
+        duration: 600
+      }
+    });
+ 
+    $( '#opener' ).on( 'click', function() {
+      $( '#dialog' ).dialog( 'open' );
+    });
+  } );
+
+
